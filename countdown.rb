42554c6543c2df4c3bebd3fc3ada976b314607e9
The Natural <philip.smalls86@gmail.com>
2019-05-19 04:02:15 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
-#write your code here
+def countdown(number)
+  while number > 0 
+  puts "#{number} SECOND(S)!"
+  number -= 1 
+  end
+  "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(number)
+  sleep(5)
 end
